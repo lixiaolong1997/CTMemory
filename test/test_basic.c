a159feb0f1a1c87a2fc52af5b77663b05207e3f7
CasaTaloyum <casatwy@msn.com>
2015-02-01 13:01:16 +0800

    finished with create memory

--- test/test_basic.c
+++ test/test_basic.c
@@ -5,9 +5,6 @@
 #include "greatest.h"
 #include "CTMemory.h"
 
-/* extern struct CTMemoryRecord; */
-/* extern struct CTMemoryRecordList; */
-
 extern struct CTMemoryRecord * _findRecord(void *recordedMemoryPointer);
 extern struct CTMemoryRecordList * _sharedMemoryRecordList(void);
 extern struct CTMemoryRecord * _createCTMemoryRecord(void *recordedMemoryPointer);
@@ -32,41 +29,12 @@ TEST test_ctRetain_ctAlloc_ctRelease(void)
     struct test_CTDerivedStruct *item = (struct test_CTDerivedStruct *)ctAlloc(sizeof(struct test_CTDerivedStruct));
     item->data = 10;
     item->pointer = (int *)ctAlloc(1);
+    *(item->pointer) = 1;
     releaseCTDerivedStruct(item);
-
-    ASSERT(item->pointer == NULL);
-    ASSERT(item == NULL);
-
-    PASS();
-}
-
-TEST test_private_methods(void)
-{
-    struct CTMemoryRecordList *list = NULL;
-    list = _sharedMemoryRecordList();
-    ASSERT(list != NULL);
-
-    struct test_CTDerivedStruct *item = (struct test_CTDerivedStruct *)ctAlloc(sizeof(struct test_CTDerivedStruct));
-    item->data = 10;
-    item->pointer = (int *)ctAlloc(1);
-    releaseCTDerivedStruct(item);
-
-    struct CTMemoryRecord *record = NULL;
-    record = _findRecord(item);
-    ASSERT(record != NULL);
-
-    record = NULL;
-    record = _createCTMemoryRecord((void *)0x12345);
-    ASSERT(record != NULL);
-    ASSERT(record->recordedMemoryPointer == 0x12345);
-
-    _deleteRecord(record);
-    ASSERT(record == NULL);
 
     PASS();
 }
 
 SUITE(test_basic) {
     RUN_TEST(test_ctRetain_ctAlloc_ctRelease);
-    RUN_TEST(test_private_methods);
 }
